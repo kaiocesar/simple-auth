c57ba46c211c35df66aad94b0c450de1bbbd58ff
Kaio Cesar <programador.kaio@gmail.com>
2014-09-02 03:25:02 -0300

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,50 @@
+module.exports = function(app, passport) {
+	// GET
+	app.get('/', function(req,res){
+		res.render('index');
+	});
+
+	app.get('/login', function(req,res){
+		res.render('login', {message: req.flash('loginMessage')});
+	});
+
+	app.get('/signup', function(req,res){
+		res.render('signup', {message: req.flash('signupMessage')});
+	});
+
+	app.get('/profile', function(req,res){
+		res.render('profile', {
+			user: req.user // get the user out of session and pass to template
+		});
+	});
+
+	app.get('/logout', function(req,res){
+		req.logout();
+		res.redirect('/');
+	});
+
+
+	// POST
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash    : true
+	}));
+
+
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile',
+		failureRedirect : '/login',
+		failureFlash    : true
+	}));
+};
+
+
+
+// functions
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	} 
+	res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+// Get all the tools we need
+var express  = require('express');
+var app = express();
+var port = process.env.PORT || 1337;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var configDB = require('./config/database.js');
+
+
+
+// configurations
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport); // pass passport for configuration
+
+
+// set up our express application
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine','ejs');
+
+
+// require for passport
+app.use(session({ secret: 'NiucsxNe.JuoYktWMBCgYrpITVR0r'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port' + port);
